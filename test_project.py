129cb813574c54a4685f46b601a4b5b73eeb77bc
Dhいa <142753318+Dhia-naouali@users.noreply.github.com>
2024-02-05 20:24:29 +0100

    Create test_project.py

---
+++ test_project.py
@@ -0,0 +1,53 @@
+import student, pytest, data
+from project import rank, display
+
+def test_Weights():
+    # Weights is a class in the student module
+    with pytest.raises(ValueError):
+        student.Weights(0.5, 0.3, 0.1)
+        student.Weights(-0.1, 0.7, 0.4)
+
+def test_subject():
+    # subject is a class in the student module
+    with pytest.raises(ValueError):
+        student.Subject("subject", "unit", -1, student.Weights(0.5, 0.4, 0.1))
+        student.Subject("subject", "unit", 0, student.Weights(0.5, 0.4, 0.1))
+
+
+def test_student():
+    # student is a class in the student module
+    global student
+    s = student.Student("levi", "clark")
+    assert(s.get_subject_score("english")) == 14.6
+    assert(s.score(mode="invalid")) == 0
+    with pytest.raises(ValueError):
+        s.get_subject_score("invalid_subject")
+    s = student.Student("chloe", "foster")
+    assert(rank(s)) == (1, [])
+    s.gradesbook = {}
+    assert(s.report()) == f"\n\n{student.Color.blue}scores report {{    \n{student.Color.blue}}}{student.Color.reset}\n"
+
+
+def test_data():
+    assert(data.get_score("levi  ", "  clark", "english")) == [18, 9.5, 18]
+    with pytest.raises(ValueError):
+        data.get_average("invalid", student.Weights(0.2, 0.2, 0.6))
+
+
+def test_display():
+    list = [
+        {"name": "john harvard", "score": 20},
+        {"name": "bruno mars", "score": 18},
+        {"name": "marques brownee", "score": 19},
+        {"name": "david milan", "score": 16},
+              ]
+    higher = display(list, colored=False)
+    assert(next(higher)) == "01 : ---john harvard : 20.00"
+    assert(next(higher)) == "02 : marques brownee : 19.00"
+    assert(next(higher)) == "03 : -----bruno mars : 18.00"
+    assert(next(higher)) == "04 : ----david milan : 16.00"
+
+
+def test_rank():
+    assert(rank(student.Student("chloe", "foster"))) == (1, [])
+    assert(rank(student.Student("ethan", "sullivan"))) == (2, [{"name": "Chloe Foster", "score": 13.71}])
