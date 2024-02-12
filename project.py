6ff2471ca6cd1224b6c7c1e1844cd3426999b73f
Dhいa <142753318+Dhia-naouali@users.noreply.github.com>
2024-02-12 09:52:22 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,26 +1,28 @@
-from data import grades, titles
-from student import Student, Color as c
+from classes import Color, Student
 import sys, inflect
 p = inflect.engine()
-
+from data import titles, grades
 
 def main():
-    print("submitted subjects:", p.join(titles))
+    print(f"{Color.red}submitted subjects:{Color.blue}", p.join(titles), f"{Color.reset}\n")
     try:
-        first, last = input(f"{c.red}Student{{\n{c.red}   -------------- full name: {c.reset}").strip().split()
+        first, last = input(f"{Color.red}Student{{\n{Color.red}   -------------- full name: {Color.reset}").strip().split()
     except ValueError:
         sys.exit("invalid name, input you first and last name")
-
-    student = Student(first, last)
-    # add rank manually along with the report
-    print(student)
-    r, sup = rank(student)
-    print(f"   {c.red}---------------- ranking: {c.reset}{p.ordinal(r)}\n{c.red}}}{c.reset}")
-
-    print(student.report())
-    higher = display(sup)
-    for student in higher:
+    try:
+        student = Student(first, last)
+        # add rank manually along with the report
         print(student)
+        r, sup = rank(student)
+        print(f"   {Color.red}---------------- ranking: {Color.reset}{p.ordinal(r)}\n{Color.red}}}{Color.reset}")
+        print(student.report())
+        print(average_student())
+        higher = display(sup)
+        for student in higher:
+            print(student)
+    except ValueError as ve:
+        sys.exit(str(ve))
+
 
 def display(d, colored=True):
     try:
@@ -29,7 +31,7 @@ def display(d, colored=True):
         print("you're the best ⭐\n")
         return None
     for index, s in enumerate(sorted(d, key=lambda s: s["score"], reverse=True)):
-        yield (f"{index+1:02} : {s['name'].rjust(m, '-')} : {c.red if colored else ''}{s['score']:.2f}{c.reset if colored else ''}")
+        yield (f"{index+1:02} : {s['name'].rjust(m, '-')} : {Color.red if colored else ''}{s['score']:.2f}{Color.reset if colored else ''}")
 
 def rank(user):
     rank = 1
@@ -43,6 +45,14 @@ def rank(user):
             sup.append({"name": f"{student.first} {student.last}", "score":round(test_score, 2)})
             rank += 1
     return rank, sup
+
+def average_student():
+    total = [Student(name[0], name[1]).score(mode="proportional") for name in grades]
+    n = len(total)
+    total = sum(total)
+    return(f"{Color.blue}{{\n    average student score: {Color.red}{total/n:.2f}\n{Color.blue}}}{Color.reset}\n")
+
+
 
 
 if __name__ == "__main__":
